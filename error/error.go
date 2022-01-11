6d9ba69f138e34cd12954d357ef682975b1f12d2
Mradul Rathore <er.mradulrathore@gmail.com>
2022-01-11 12:50:11 +0530

    add custom error

---
+++ error/error.go
@@ -0,0 +1,10 @@
+package error
+
+import "errors"
+
+var (
+	NoItmErr         = errors.New("pleae specify item type")
+	NegativeQuantErr = errors.New("quantity can not be negative")
+	NegativePriceErr = errors.New("price can not be negative")
+	InvalideItmType  = errors.New("item type can only be raw, manufactured or imported")
+)
